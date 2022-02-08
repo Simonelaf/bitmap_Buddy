2afeca50a3f73894b020b986d7a63ffe067417e7
Simone La falce <lafalce.1534708@studenti.uniroma1.it>
2022-02-08 13:56:27 +0100

    piccole modifiche al file del prof

--- buddy_allocator.h
+++ buddy_allocator.h
@@ -1,35 +1,42 @@
 #pragma once
-#include "bitmap.h"
+#include "bit_map.h"
 
 #define MAX_LEVELS 16
 
 //il buddy allocator ha la stessa struttura della versione con le liste
 //uso bitmap invece delle liste
-typedef struct{
-    BitMap bitmap;
-    int num_levels;
-    char* buffer; // the buffer area to be managed
-    int min_bucket_size; // the minimum page of RAM that can be returned
+typedef struct  {
+	//I just need a bitmap to handle the allocator
+	BitMap* tree;
+	int num_levels;
+	char* memory; // the memory area to be managed
+	int min_bucket_size; // the minimum size page of RAM that can be returned
 } BuddyAllocator;
+
 
 // computes the size in bytes for the buffer of the allocator
 int BuddyAllocator_calcSize(int num_levels);
 
-// initializes the buddy allocator, and checks that the buffer is large enough
-void BuddyAllocator_init(BuddyAllocator* alloc, int num_levels, char* buffer, int buffer_size, char* memory, int min_bucket_size);
 
-// returns (allocates) a buddy at a given level.
-// side effect on the internal structures
+// initializes the buddy allocator, and checks that the buffer is large enough
+void BuddyAllocator_init(BuddyAllocator* alloc,
+                         int num_levels,
+                         char* buffer,
+                         int buffer_size,
+                         char* memory,
+                         int min_bucket_size);
+                         
+// returns the index in the bitmap of the buddy
 // 0 id no memory available
 int BuddyAllocator_getBuddy(BuddyAllocator* alloc, int level);
 
 
 // releases an allocated buddy, performing the necessary joins
 // side effect on the internal structures
-void BuddyAllocator_releaseBuddy(BuddyAllocator* alloc, int buddy);
+void BuddyAllocator_releaseBuddy(BuddyAllocator* alloc, int idx);
 
-// allocates memory
-void *BuddyAllocator_malloc(BuddyAllocator* alloc, int size);
+//allocates memory
+void* BuddyAllocator_malloc(BuddyAllocator* alloc, int size);
 
-// releases allocated memory
+//releases allocated memory
 void BuddyAllocator_free(BuddyAllocator* alloc, void* mem);
